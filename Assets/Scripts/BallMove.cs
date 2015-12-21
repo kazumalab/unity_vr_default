80f66d07ca6688c3a53159a51d0a8626c37ff427
Kazuma <kazuma-arashi@hotmail.co.jp>
2015-12-21 12:42:53 +0900

    fitst commit

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour {
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce (transform.forward * 1000f);
+	}
+}
---
+++ Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCamera : MonoBehaviour {
+	public Transform headChild;
+	public GameObject ball;
+	private bool shooting = false;
+	private float bulletsPerSecond = 14.0f;
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		InvokeRepeating("shot", 0.0f, 1.0f / bulletsPerSecond);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		shooting = false;
+		Vector2 mouseSpeed = new Vector2 (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"));
+		//ここは頭の動きを見る、パソコン用
+		transform.Rotate (0, mouseSpeed.x, 0);
+		headChild.Rotate (-mouseSpeed.y, 0, 0);
+
+		if (Input.GetButton("Jump")) {
+			shooting = true;
+		}
+	}
+
+	void shot(){
+		if (!shooting)return;
+		Instantiate(ball,new Vector3(transform.position.x,transform.position.y,transform.position.z),this.gameObject.transform.rotation);
+	}
+}
